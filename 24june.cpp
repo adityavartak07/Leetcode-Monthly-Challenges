0565c5e764ef1d45da251b3943801d1966e4e7be
Aditya Vartak <adityavartak1001@gmail.com>
2022-06-25 00:01:14 +0530

    24june

---
+++ 24june.cpp
@@ -0,0 +1,42 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+//Construct Target Array With Multiple Sums  Priority queue
+
+
+class Solution {
+public:
+    bool isPossible(vector<int>& target) {
+        priority_queue<int> pq;
+        long long int sum = 0;
+        int n = target.size();
+        for(auto x: target)
+        {
+            sum += x;
+            pq.push(x);
+        }
+        
+        while(pq.top()>1)
+        {
+            sum -= pq.top();
+            if(sum>pq.top() || sum==0)
+                return false;
+            
+            
+            int a = pq.top()%sum;
+            if(a==0 && sum!=1)
+                return false;
+            
+            pq.pop();
+            pq.push(a);
+            sum += a;
+        }
+        
+        return true;
+    }
+};
+
+
+
+
